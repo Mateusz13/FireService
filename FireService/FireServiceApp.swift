b916a32add346dc2b524ef3a2315555969a885ee
Mateusz Szafarczyk <szafarczyk.mat@gmail.com>
2023-02-10 21:33:14 +0100

    Initial Commit

---
+++ FireService/FireServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireServiceApp.swift
+//  FireService
+//
+//  Created by Mateusz Szafarczyk on 10/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct FireServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
