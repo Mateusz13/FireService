f918548370c443b3139c24d797e29b7691e9276f
Mateusz Szafarczyk <szafarczyk.mat@gmail.com>
2023-02-13 20:06:31 +0100

    creat folder and first MainTableModel

---
+++ FireService/AllViews/Core/Views/MainTableView.swift
@@ -0,0 +1,20 @@
+//
+//  MainTableView.swift
+//  FireService
+//
+//  Created by Mateusz Szafarczyk on 13/02/2023.
+//
+
+import SwiftUI
+
+struct MainTableView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainTableView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTableView()
+    }
+}
